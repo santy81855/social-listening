de02815aa48ab0a1b4a2f0feeafd67d13eb189b2
root <root@DESKTOP-7KIGB8O>
2023-12-08 16:20:59 -0500

    changed background color

--- src/components/report/PieChartPadding.jsx
+++ src/components/report/PieChartPadding.jsx
@@ -2,13 +2,6 @@
 import React, { useState } from 'react';
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
 
-const data = [
-    { name: 'Group A', value: 400 },
-    { name: 'Group B', value: 300 },
-    { name: 'Group C', value: 300 },
-    { name: 'Group D', value: 200 },
-];
-
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 const RADIAN = Math.PI / 180;
@@ -26,7 +19,7 @@ const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, per
 
 const PieChartPadding = ({ reportData }) => {
     return (
-        <ResponsiveContainer width="100%" height="100%" style={{ backgroundColor: "transparent" }}>
+        <ResponsiveContainer width="100%" height="100%">
             <PieChart >
                 <Pie
                     data={reportData}
@@ -38,7 +31,7 @@ const PieChartPadding = ({ reportData }) => {
                     fill="#8884d8"
                     dataKey="value"
                 >
-                    {data.map((entry, index) => (
+                    {reportData.map((entry, index) => (
                         <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                     ))}
                 </Pie>
