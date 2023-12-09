7a0305c6e38f4f39f69cc94af41a61a7d72f2d44
root <root@DESKTOP-7KIGB8O>
2023-12-08 19:23:22 -0500

    agregar landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,45 @@
 import styles from './page.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home({ searchParams }) {
     return (
         <main className={styles.main}>
-            Home Page
+            <section className={styles.heroSection}>
+                <section className={styles.heroTextContainer}>
+                    <section className={styles.heroText}>
+                        <h1 className={styles.title}>Social Listening: Datos de Sentimientos y Engagement en Tiempo Real</h1>
+                        <p className={styles.description}>Descubre lo que dicen las redes sociales. Introduce hashtags y obtén valiosos datos sobre sentimientos, interacciones y participación en tiempo real.</p>
+                    </section>
+                    <Link href="/generate-report" className={styles.actionButton}>Crear Reporte</Link>
+                </section>
+                <section className={styles.heroImageContainer}>
+                    <Image
+                        src="/images/landing-page/dashboard-icon.png"
+                        alt="Hero Image"
+                        width={250}
+                        height={200}
+                        className={styles.heroImage}
+                    />
+                </section>
+            </section>
+            <section className={styles.featuresCardContainer}>
+                <section className={styles.featuresCard}>
+                    <i className={`fa-regular fa-heart`}></i>
+                    <h2 className={styles.featuresCardTitle}>Sentimiento</h2>
+                    <p className={styles.featuresCardDescription}>Descubre cómo se sienten los usuarios de las redes sociales sobre un tema en específico. Nuestro sistema analiza los sentimientos de los usuarios en tiempo real.</p>
+                </section>
+                <section className={styles.featuresCard}>
+                    <i className={`fa-regular fa-thumbs-up`}></i>
+                    <h2 className={styles.featuresCardTitle}>Interacción</h2>
+                    <p className={styles.featuresCardDescription}>Descubre cuántas personas están hablando sobre un tema en específico. Nuestro sistema analiza la participación de los usuarios.</p>
+                </section>
+                <section className={styles.featuresCard}>
+                    <i className={`fa-regular fa-clock`}></i>
+                    <h2 className={styles.featuresCardTitle}>Tiempo Real</h2>
+                    <p className={styles.featuresCardDescription}>Obtén datos al instante. Personaliza tu análisis seleccionando un periodo, ya sea del pasado o del presente</p>
+                </section>
+            </section>
         </main>
     )
 }
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -4,19 +4,21 @@ import Link from 'next/link';
 const Navbar = () => {
     return (
         <nav className={styles.navContainer}>
-            <div className={styles.navItem}>
-                <Link href="/">
-                    <p className={styles.companyName}>ASResearch</p>
-                </Link>
-            </div>
-            <div className={styles.navItem}>
-                <Link href="/">
-                    <p className={styles.navLink}>Home</p>
-                </Link>
-                <Link href="/generate-report">
-                    <p className={styles.reportButton}>Generate Report</p>
-                </Link>
-            </div>
+            <section className={styles.textContainer}>
+                <div className={styles.navItem}>
+                    <Link href="/">
+                        <p className={styles.companyName}>ASResearch</p>
+                    </Link>
+                </div>
+                <div className={styles.navItem}>
+                    <Link href="/">
+                        <p className={styles.navLink}>Inicio</p>
+                    </Link>
+                    <Link href="/generate-report">
+                        <p className={styles.reportButton}>Crear Reporte</p>
+                    </Link>
+                </div>
+            </section>
         </nav>
     );
 
